f7e995805c283cbdb6d4743aa2c98be0366fc75b
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-09-28 19:10:30 -0700

    Create 10.c

---
+++ 10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],b[10],i;
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=9;i++)
+    {
+        b[i]=a[i];
+    }
+    for(i=0;i<=9;i++)
+    {
+        printf("%d",b[i]);
+    }
+    return 0;
+
+}
