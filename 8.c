9446db7fbe72427df30cc3e38866e1d3c7547552
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-09-28 19:10:52 -0700

    Create 8.c

---
+++ 8.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],c,i=0,j;
+    printf("enter 10 numbers : ");
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(j=0;a[10]>=a[1];)
+    {
+        c=0;
+        if(a[j]<=a[j+1])
+        {
+            j++;
+        }
+        else
+        {
+            c=a[j];
+            a[j]=a[j+1];
+            a[j+1]=c;
+            j=0;
+            
+            
+            
+            
+
+        }
+    }
+    
+   
+        printf("second smallest number is %d ",a[1]);
+    
+    return 0;
+}
