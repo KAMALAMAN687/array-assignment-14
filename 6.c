ade1dc3b9e197ef29a6acf7c89c826de2de11902
Aman Kamal <109606739+KAMALAMAN687@users.noreply.github.com>
2022-09-28 19:10:42 -0700

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],c,i=0,j;
+    printf("enter 10 numbers : ");
+    for(i=0;i<=9;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    
+    for(j=0;a[10]>=a[1];)
+    {
+        c=0;
+        if(a[j]<=a[j+1])
+        {
+            j++;
+        }
+        else
+        {
+            c=a[j];
+            a[j]=a[j+1];
+            a[j+1]=c;
+            j=0;
+            
+            
+            
+            
+
+        }
+    }
+    
+    for(i=0;i<=9;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
